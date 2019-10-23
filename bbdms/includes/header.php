8928ae6ce2f088d81dc501d4ac51ad915f3e2f6a
architjain-12 <architjain08@gmail.com>
2019-10-23 14:51:12 +0530

    Final Files

---
+++ bbdms/includes/header.php
@@ -0,0 +1,46 @@
+    <nav class="navbar fixed-top navbar-toggleable-md navbar-inverse bg-inverse">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarExample" aria-controls="navbarExample" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="container">
+            <a class="navbar-brand" href="index.php">Save a Life</a>
+            <div class="collapse navbar-collapse" id="navbarExample">
+                <ul class="navbar-nav ml-auto">
+                    <!--<li class="nav-item">
+                        <a class="nav-link" href="page.php?type=aboutus">About</a>
+                    </li> -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="page.php?type=donor">Why Become Donor</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="become-donar.php">Donate</a>
+                    </li>
+                 
+                     <li class="nav-item">
+                        <a class="nav-link" href="search-donor.php">Search Donor</a>
+                    </li>
+                
+                      <li class="nav-item">
+                        <a class="nav-link" href="./admin/index.php">Admin</a>
+                    </li>
+               <!--
+
+                 <li class="nav-item">
+                        <a class="nav-link" href="#">Why Become Donor</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Donate</a>
+                    </li>
+                 
+                     <li class="nav-item">
+                        <a class="nav-link" href="#">Search Donor</a>
+                    </li>
+                
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Admin</a>
+                    </li>
+                 `-->
+                </ul>
+            </div>
+        </div>
+    </nav>
